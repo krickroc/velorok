2130ed40c751dd9b454428101284498eddfa455f
Julien Catala <julien.catala@ext.consilium.europa.eu>
2020-06-25 09:50:06 +0200

    Basic auth

---
+++ api/src/test/java/velorok/api/VelorokApiApplicationTests.java
@@ -0,0 +1,13 @@
+package velorok.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VelorokApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
